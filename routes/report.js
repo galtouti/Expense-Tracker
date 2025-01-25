b4d1b7058466ad02e7ba936f64e7286fd6cccb10
Saharaxsus <97562695+SaharAb1@users.noreply.github.com>
2025-01-25 20:16:54 +0200

    Updated Report to match for categories also

--- routes/report.js
+++ routes/report.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Cost = require('../models/cost');
 
-// דוח חודשי
+// Monthly report endpoint with grouping by categories
 router.get('/report', async (req, res) => {
   const { id, year, month } = req.query;
 
@@ -14,10 +14,16 @@ router.get('/report', async (req, res) => {
     const startDate = new Date(`${year}-${month}-01`);
     const endDate = new Date(`${year}-${parseInt(month) + 1}-01`);
 
-    const costs = await Cost.find({
-      userid: id,
-      date: { $gte: startDate, $lt: endDate },
-    });
+      const costs = await Cost.aggregate([
+          { $match: { userid: id, date: { $gte: startDate, $lt: endDate } } },
+          {
+              $group: {
+                  _id: "$category",
+                  totalSum: { $sum: "$sum" },
+                  items: { $push: { description: "$description", sum: "$sum", date: "$date" } }
+              }
+          }
+      ]);
 
     res.status(200).json(costs);
   } catch (error) {
