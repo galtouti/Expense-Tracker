a95266ca87c98eb2d45a0684a84ca56f7de1ec0c
GalTouti <toutigal@gmail.com>
2025-01-25 21:35:25 +0200

    Get to costs

--- routes/costs.js
+++ routes/costs.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../models/user');
 const router = express.Router();
 const Cost = require('../models/cost');
 
@@ -12,6 +13,36 @@ router.post('/add', async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+});
+
+// בקשת GET לקבלת כל ההוצאות
+router.get('/', async (req, res) => {
+  try {
+    const costs = await Cost.find();
+    res.status(200).json(costs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// בקשת POST ליצירת משתמש חדש
+router.post('/users', async (req, res) => {
+    const { id, first_name, last_name, birthday, marital_status } = req.body;
+
+    const newUser = new User({
+        id,
+        first_name,
+        last_name,
+        birthday,
+        marital_status,
+    });
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
 module.exports = router;
